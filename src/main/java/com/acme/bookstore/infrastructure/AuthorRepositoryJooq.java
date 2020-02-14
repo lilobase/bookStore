b2a5ad680302d447805b6df4fc0a6973337672af
Arnaud LEMAIRE <arnaud@legolas.exchange>
2020-02-14 10:37:14 +0100

    test(persistence): add test for AuthorRepository

--- src/main/java/com/acme/bookstore/infrastructure/AuthorRepositoryJooq.java
+++ src/main/java/com/acme/bookstore/infrastructure/AuthorRepositoryJooq.java
@@ -8,7 +8,6 @@ import org.jooq.impl.DSL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.io.Serializable;
 import java.util.UUID;
 
 @Component
---
+++ src/test/java/com/acme/bookstore/infrastructure/AuthorRepositoryJooqTest.java
@@ -0,0 +1,23 @@
+package com.acme.bookstore.infrastructure;
+
+import com.acme.bookstore.domain.Author;
+import infrastructure.InMemoryDSLContext;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthorRepositoryJooqTest {
+    @Test
+    void itPersists() {
+        final AuthorRepositoryJooq repository = new AuthorRepositoryJooq(InMemoryDSLContext.DSL());
+
+        final Author expectedAuthor = new Author(UUID.randomUUID(), "Arnaud");
+        repository.add(expectedAuthor);
+
+        final Author actualAuthor = repository.get(expectedAuthor.id());
+        assertThat(actualAuthor).isEqualToComparingFieldByField(expectedAuthor);
+    }
+}
